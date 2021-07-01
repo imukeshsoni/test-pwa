d66efa3320efd7393c21733975fbd0db72d35c3a
mukesh <msony552@gmail.com>
2021-07-01 13:38:02 +0530

    custom caching implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent  {
-  message:any;
-
-
-  ngOnInit(){
-    
-    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(
-      (res: any) => {
-        this.message = JSON.stringify(res);
-        console.log(res);
-      },err => {
-        console.error(err);
-      }
-    )
-  }
+  
   title = 'test-pwa';
+  message:any;
 
   async cacheMyData() {
     var url = 'https://qa.njdep.arinspect.com/api/auth-service/v2/login';
-    // var customCache = await caches.open('my-cache');
+    var customCache = await caches.open('my-cache');
     this.http
       .post(
         url,
@@ -41,9 +29,7 @@ export class AppComponent  {
       )
       .subscribe(async (res) => {
         console.log(res);
-        this.message = res.toString();
-        //customCache.add(request, response)
-        // customCache.put('token', new Response(new Blob([JSON.stringify(res)])));
+        customCache.put('token', new Response(new Blob([JSON.stringify(res)])));
 
       });
   }
@@ -51,21 +37,21 @@ export class AppComponent  {
   async dataCached() {
     this.cacheMyData();
 
-    // var customCache = await caches.open('my-cache');
+    var customCache = await caches.open('my-cache');
 
-    // await customCache
-    //   .match('token')
-    //   .then((cacheResponse) => {
-    //     cacheResponse.text().then((t) => {
-    //       this.message = 'Data is already cached';
-    //       console.log(JSON.parse(t))
-    //       return;
-    //     });
-    //   })
-    //   .catch((err) => {
-    //     console.log("No Internet connection.");
-    //     this.cacheMyData();
-    //   });
+    await customCache
+      .match('token')
+      .then((cacheResponse) => {
+        cacheResponse.text().then((t) => {
+          this.message = 'Data is already cached';
+          console.log(JSON.parse(t))
+          return;
+        });
+      })
+      .catch((err) => {
+        console.log("No Internet connection.");
+        this.cacheMyData();
+      });
   }
   constructor(private http: HttpClient) {}
 }
